3072f9125d9edecdf61cfd134c1e82d77c018f61
BradenE19 <6115@holbertonstudents.com>
2024-04-16 12:45:10 -0500

    update html link

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'custom-api')));
 
 // Route to serve the HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'test.html'));
 });
 
 // Route to serve the CSS file
